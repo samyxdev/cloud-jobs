477078dfd6f8068521e232d4b72c9842f98af44a
DanielAR <daniel.arias@skyline.be>
2022-05-14 10:24:25 +0200

    Update textract_CV.py

--- Textract/textract_CV.py
+++ Textract/textract_CV.py
@@ -1,7 +1,80 @@
-
 import boto3
 import io
+import sys
+import re
+import json
+
 from PIL import Image, ImageDraw
+
+
+def get_kv_map(document, client):
+
+    response = client.analyze_document(Document={'Bytes': document}, FeatureTypes=['FORMS'])
+
+    # Get the text blocks
+    blocks=response['Blocks']
+    
+
+    # get key and value maps
+    key_map = {}
+    value_map = {}
+    block_map = {}
+    for block in blocks:
+        block_id = block['Id']
+        block_map[block_id] = block
+        if block['BlockType'] == "KEY_VALUE_SET":
+            if 'KEY' in block['EntityTypes']:
+                key_map[block_id] = block
+            else:
+                value_map[block_id] = block
+
+    return key_map, value_map, block_map
+
+
+def get_kv_relationship(key_map, value_map, block_map):
+    kvs = {}
+    for block_id, key_block in key_map.items():
+        value_block = find_value_block(key_block, value_map)
+        key = get_text(key_block, block_map)
+        val = get_text(value_block, block_map)
+        kvs[key] = val
+    return kvs
+
+
+def find_value_block(key_block, value_map):
+    for relationship in key_block['Relationships']:
+        if relationship['Type'] == 'VALUE':
+            for value_id in relationship['Ids']:
+                value_block = value_map[value_id]
+    return value_block
+
+
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] == 'SELECTED':
+                            text += 'X '    
+
+                                
+    return text
+
+
+def print_kvs(kvs):
+    for key, value in kvs.items():
+        print(key, ":", value)
+
+
+def search_value(kvs, search_key):
+    for key, value in kvs.items():
+        if re.search(search_key, key, re.IGNORECASE):
+            return value
 
 def draw_bounding_box(key, val, width, height, draw):
     # If a key is Geometry, draw the bounding box info in it
@@ -37,14 +110,26 @@ def process_text_detection(bucket, document):
     s3_object = s3_connection.Object(bucket, document)
     s3_response = s3_object.get()
 
-    # opening binary stream using an in-memory bytes buffer
-    stream = io.BytesIO(s3_response['Body'].read())
+    # with open(s3_response['Body'], 'rb') as file:
+    #     img_test = file.read()
+    #     print('Image loaded', file_name)
 
-    # loading stream into image
-    image = Image.open(stream)
+    # # opening binary stream using an in-memory bytes buffer
+    # stream = io.BytesIO(s3_response['Body'].read())
 
-    # Detect text in the document
-    client = boto3.client('textract', region_name="us-east-1")
+    # # loading stream into image
+    # image = Image.open(stream)
+
+    # # Detect text in the document
+    client = boto3.client('textract', region_name="eu-west-1")
+
+    bytes_test = bytearray(s3_response['Body'].read())
+    key_map, value_map, block_map = get_kv_map(bytes_test, client)
+
+    # Get Key Value relationship
+    kvs = get_kv_relationship(key_map, value_map, block_map)
+    print("\n\n== FOUND KEY : VALUE pairs ===\n")
+    print_kvs(kvs)
 
     # process using S3 object
     response = client.analyze_expense(
@@ -85,9 +170,9 @@ def process_text_detection(bucket, document):
 
 def main():
     bucket = 'sagemaker-canvas-bucket-tutorial'
-    document = 'CV.png'
-    process_text_detection(bucket, document)
+    document = 'Daniel_Arias_CV.pdf'
+    
+    process_text_detection(bucket, document)    
 
 if __name__ == "__main__":
-    main()
-                  
+    main()                  
