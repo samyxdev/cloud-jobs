7e1e01650f53f93b1265e5a84d269f6171aedf73
Samy Chouiti <samy.chouiti@grenoble-inp.org>
2022-05-22 13:40:58 +0200

    Adding Job retrieval from DB. Now missing real fields from the DB

--- cv_site/cv_match/views.py
+++ cv_site/cv_match/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Jobs
 
-# Hardcoded listenings
-jobs = [{"title":"Software Engineer", "desc":"Super position in a super company"}, {"title":"Smart Contract Dev", "desc":"Solidity expert with 2+ years experience related to SC developpment."}]
+# Hardcoded listenings for example
+hardcoded_jobs = [{"title":"Software Engineer", "desc":"Super position in a super company", "date":"22/05/2022"}, {"title":"Smart Contract Dev", "desc":"Solidity expert with 2+ years experience related to SC developpment.", "date":"20/05/2022"}]
 
 def index(request):
-    context = {"jobs":jobs}
+    #jobs = Jobs()
+    #context = {"jobs":jobs.get_jobs()}
+    context = {"jobs":hardcoded_jobs}
+
     return render(request, 'cv_match/index.html', context)
 
 def upload(request):
