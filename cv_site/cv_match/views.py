4e121be99789c384f40fa20cee1acba1cf0156a0
Samy Chouiti <samy.chouiti@grenoble-inp.org>
2022-05-23 12:51:58 +0200

    Adding skills display

--- cv_site/cv_match/views.py
+++ cv_site/cv_match/views.py
@@ -6,12 +6,16 @@ from .models import Jobs
 hardcoded_jobs = [{"title":"Software Engineer",
                     "company":"TrustEn",
                     "desc":"Super position in a super company",
-                    "date":"22/05/2022"},
+                    "date":"22/05/2022",
+                    "location":"Barcelona",
+                    "skills":["Ruby", "Python"]},
 
                     {"title":"Smart Contract Dev",
                     "company":"Slope.fi",
                     "desc":"Solidity expert with 2+ years experience related to SC developpment.",
-                    "date":"20/05/2022"}]
+                    "date":"20/05/2022",
+                    "location":"Remote",
+                    "skills":["Solidity"]}]
 
 def index(request):
     #jobs = Jobs()
