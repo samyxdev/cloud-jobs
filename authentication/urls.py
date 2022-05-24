849de1969463f8d0a3908fb4eb6d503eacf39ed7
francescoari <aristei37@gmail.com>
2022-05-24 18:26:16 +0200

    restored

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,10 +5,11 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index , name='index'),
-    path('register', views.register, name='register' ),
+    #path('login', views.index , name='login'),
+    path('register', views.register, name='register'),
     path('home',views.home , name= 'home'),
     path('cv', views.cv, name='cv' ),
     re_path(r'^logout/$', views.logout, name='logout'),
-    path('savejob', views.save, name='savejob')
-    
+    path('savejob', views.save, name='savejob'),
+    #path('upload', views.upload, name='upload')
 ]
